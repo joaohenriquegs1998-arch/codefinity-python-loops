041e90eb29c1bfbbfe1b1e1212bb27f5d62b67e5
joaohenriquegs1998-arch <>
2026-04-23 08:10:27 +0000

    Enumerate function

---
+++ section/enumerate_function/main.py
@@ -0,0 +1,14 @@
+# Full list of countries you are considering for travel
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# Initialize an empty list for rankings
+travel_rankings = []
+for index, values in enumerate(countries):
+    index = index + 1
+    travel_rankings.append(f"Destination {index}: {values}")
+
+# Use enumerate to label each destination
+
+
+# Testing
+print("Travel destination rankings:", travel_rankings)
